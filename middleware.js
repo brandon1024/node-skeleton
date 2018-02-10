d8f0a6124470b2de1112ce9bf16a543e90eb28af
Jacob Smith <jacob.smith@unb.ca>
2018-02-10 17:47:31 -0400

    finished login

--- middleware.js
+++ middleware.js
@@ -3,8 +3,11 @@ var express  = require('express');
 var models   = require('./models');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
+var expressSession = require('express-session');
 var bodyParser = require('body-parser');
 var sassMiddleware = require('node-sass-middleware');
+var flash = require("connect-flash");
+var passport = require('passport');
 
 module.exports = function (app) {
     // uncomment after placing your favicon in /public
@@ -13,6 +16,8 @@ module.exports = function (app) {
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: false }));
     app.use(cookieParser());
+    app.use(passport.initialize());
+    app.use(expressSession({secret: 'keyboard cat', cookie: {}}));
     app.use(sassMiddleware({
         src: path.join(__dirname, 'public'),
         dest: path.join(__dirname, 'public'),
@@ -20,6 +25,7 @@ module.exports = function (app) {
         sourceMap: true
     }));
     app.use(express.static(path.join(__dirname, 'public')));
+    app.use(flash());
     app.use(function (req, res, next) {
         models(function (err, db) {
             if (err) return next(err);
