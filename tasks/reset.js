abbf9b5ade4c8324aba82b9f88f3b441181d1d42
Jacob Smith <jacob.smith@unb.ca>
2018-02-10 15:57:31 -0400

    updates reset script

--- tasks/reset.js
+++ tasks/reset.js
@@ -12,32 +12,17 @@ models(function (err, db) {
             db.models.user.create({id: 1, username: 'jacob', password: 'hackthehack'}, function (err, user) {
                 if(err) throw err;
 
-                db.close();
-                console.log("Done!");
+                db.models.user.find({}, function (err, users) {
+                    if(err) throw err;
+
+                    db.models.channel.create({users: users}, function (err, _) {
+                        if(err) throw err;
+
+                        db.close();
+                        console.log("Done!");
+                    });
+                });
             });
-            
-            //db.models.user.create({id: 2, username: 'amir', password: 'hackthehack'});
-            //db.models.user.create({id: 3, username: 'brandon', password: 'hackthehack'});
-            //db.models.user.create({id: 4, username: 'alex', password: 'hackthehack'});
-
-
-
-            /*db.models.user.find({}, function (err, users) {
-                if(err) throw err;
-
-                db.models.message.create({
-                    title: "Hello world", body: "Testing testing 1 2 3"
-                }, function (err, message) {
-                    if (err) throw err;
-
-                    db.close();
-                    console.log("Done!");
-                });
-
-                db.models.channel.create({
-                    users: user
-                });
-            });*/
         });
     });
 });
