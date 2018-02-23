cb928dadb0375bb4faa6c9074b960575c9d9c4ed
Brandon Richardson <brandon1024.br@gmail.com>
2018-02-22 22:48:28 -0400

    added backend validation and improved frontend validation

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,6 +1,32 @@
 function validateForm() {
-    /* Validate Passwords Fields*/
+    /* Validate Username Length Constraint */
+    var username = document.getElementById('username').value;
+    if(username.length < 6 || username.length > 32) {
+        document.getElementById('username').classList.add('invalid');
+        return false;
+    }
+    else
+        document.getElementById('username').classList.remove('invalid');
+
+    /* Validate Email Length Constraint */
+    var email = document.getElementById('email').value;
+    if(email.length > 255) {
+        document.getElementById('email').classList.add('invalid');
+        return false;
+    }
+    else
+        document.getElementById('email').classList.remove('invalid');
+
+    /* Validate Password Length Constraint */
     var password = document.getElementById('password').value;
+    if(password.length > 64) {
+        document.getElementById('password').classList.add('invalid');
+        return false;
+    }
+    else
+        document.getElementById('password').classList.remove('invalid');
+
+    /* Validate Password Match */
     var passwordConfirm = document.getElementById('password-confirm').value;
     if(password !== passwordConfirm) {
         document.getElementById('password-confirm').classList.add('invalid');
@@ -9,9 +35,26 @@ function validateForm() {
     else
         document.getElementById('password-confirm').classList.remove('invalid');
 
-
     return true;
 }
+
+document.getElementById('username').addEventListener('change', function(event) {
+    var targetElement = event.target || event.srcElement;
+    if(!targetElement.value)
+        targetElement.classList.remove('invalid');
+});
+
+document.getElementById('email').addEventListener('change', function(event) {
+    var targetElement = event.target || event.srcElement;
+    if(!targetElement.value)
+        targetElement.classList.remove('invalid');
+});
+
+document.getElementById('password').addEventListener('change', function(event) {
+    var targetElement = event.target || event.srcElement;
+    if(!targetElement.value)
+        targetElement.classList.remove('invalid');
+});
 
 document.getElementById('password-confirm').addEventListener('change', function(event) {
     var targetElement = event.target || event.srcElement;
