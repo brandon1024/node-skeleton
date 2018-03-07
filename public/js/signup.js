44d555e3d2e9b2569b67d7aba53b0edb1c13b163
Brandon Richardson <brandon1024.br@gmail.com>
2018-03-06 22:58:27 -0400

    added password strength indicator

--- public/js/signup.js
+++ public/js/signup.js
@@ -38,26 +38,61 @@ function validateForm() {
     return true;
 }
 
-document.getElementById('username').addEventListener('change', function(event) {
-    var targetElement = event.target || event.srcElement;
-    if(!targetElement.value)
-        targetElement.classList.remove('invalid');
-});
+/* Returns a value from 0 to 4, with 0 being least strong */
+function passwordStrength(pass) {
+    var points = pass.match(/[a-z]/g) ? pass.match(/[a-z]/g).length : 0;
+    points += (pass.match(/[A-Z]/g) ? pass.match(/[A-Z]/g).length : 0) * 1.5;
+    points += (pass.match(/\d/g) ? pass.match(/\d/g).length : 0) * 2;
+    points += (pass.match(/[\x20-\x2F\x3A-\x40\x5B-\x60\x7B-\x7E]/g) ? pass.match(/[\x20-\x2F\x3A-\x40\x5B-\x60\x7B-\x7E]/g).length : 0) * 2.5;
 
-document.getElementById('email').addEventListener('change', function(event) {
-    var targetElement = event.target || event.srcElement;
-    if(!targetElement.value)
-        targetElement.classList.remove('invalid');
-});
+    return Math.min(4, Math.floor(points / 4));
+}
 
-document.getElementById('password').addEventListener('change', function(event) {
-    var targetElement = event.target || event.srcElement;
-    if(!targetElement.value)
-        targetElement.classList.remove('invalid');
-});
+window.onload = function() {
+    document.getElementById('username').addEventListener('change', function (event) {
+        var targetElement = event.target || event.srcElement;
+        if (!targetElement.value)
+            targetElement.classList.remove('invalid');
+    });
 
-document.getElementById('password-confirm').addEventListener('change', function(event) {
-    var targetElement = event.target || event.srcElement;
-    if(!targetElement.value)
-        targetElement.classList.remove('invalid');
-});
+    document.getElementById('email').addEventListener('change', function (event) {
+        var targetElement = event.target || event.srcElement;
+        if (!targetElement.value)
+            targetElement.classList.remove('invalid');
+    });
+
+    document.getElementById('password').addEventListener('change', function (event) {
+        var targetElement = event.target || event.srcElement;
+        if (!targetElement.value)
+            targetElement.classList.remove('invalid');
+    });
+
+    document.getElementById('password').addEventListener('input', function() {
+        /* Change Incator Bar Color */
+        var labelIndicator = document.querySelector('input[type=password]');
+        var password = document.getElementById('password').value;
+        var strength = passwordStrength(password);
+
+        var classes = labelIndicator.classList.toString().match(/strength\d/);
+        if(classes)
+            for(var index = 0; index < classes.length; index++)
+                labelIndicator.classList.remove(classes[index]);
+
+        if(strength === 0)
+            labelIndicator.classList.add('strength0');
+        else if(strength === 1)
+            labelIndicator.classList.add('strength1');
+        else if(strength === 2)
+            labelIndicator.classList.add('strength2');
+        else if(strength === 3)
+            labelIndicator.classList.add('strength3');
+        else
+            labelIndicator.classList.add('strength4');
+    });
+
+    document.getElementById('password-confirm').addEventListener('change', function (event) {
+        var targetElement = event.target || event.srcElement;
+        if (!targetElement.value)
+            targetElement.classList.remove('invalid');
+    });
+};
